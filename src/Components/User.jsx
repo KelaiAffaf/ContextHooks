684a51bc50694cc1afc8b1e35d92825ce6a771ba
Afaf KELAI <ea_kelai@esi.dz>
2022-01-18 19:27:49 +0100

    context.consumer

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -3,12 +3,19 @@ import { useContext } from 'react'
 import { UserContext } from '../Context/Context'
 function User() {
 
-    const user = useContext(UserContext)
+    // const user = useContext(UserContext)
     return (
         <div>
-
-            <h1>{user.name}</h1>
-            <h2>{user.age}</h2>
+            {/* <h1>{user.name}</h1>
+            <h2>{user.age}</h2> */}
+            <UserContext.Consumer>
+                {(user) => (
+                    <div>
+                        <p>{user.name} </p>
+                        <p>{user.age} </p>
+                    </div>
+                )}
+            </UserContext.Consumer>
 
 
         </div>
